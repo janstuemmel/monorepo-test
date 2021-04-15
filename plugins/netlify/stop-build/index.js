1395b5d46287481a19e950861e3876ffce27fb43
farlock <farlock@mediamarktsaturn.com>
2021-04-15 20:47:03 +0200

    updated plugin

--- plugins/netlify/stop-build/index.js
+++ plugins/netlify/stop-build/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  onPreBuild: () => {
+  onPreBuild: ({ utils }) => {
     const currentProject = process.env.PROJECT_NAME;
     const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
     const latestCommit = 'HEAD';
@@ -19,12 +19,9 @@ module.exports = {
 function projectChanged(currentProject, fromHash, toHash) {
   const execSync = require('child_process').execSync;
   const getAffected = `npm run --silent nx print-affected --base=${fromHash} --head=${toHash}`;
-  console.log(fromHash, toHash);
   const output = execSync(getAffected).toString();
-  console.log(output);
   //get the list of changed projects from the output
   const changedProjects = JSON.parse(output).projects;
-  console.log(changedProjects);
   if (changedProjects.find((project) => project === currentProject)) {
     return true;
   } else {
