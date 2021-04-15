be7b7034fbed6eda424954460ce09972583cfa3a
farlock <farlock@mediamarktsaturn.com>
2021-04-15 20:27:51 +0200

    updated plugin

--- plugins/netlify/stop-build/index.js
+++ plugins/netlify/stop-build/index.js
@@ -1,6 +1,13 @@
 module.exports = {
   onPreBuild: () => {
-    const currentProject = 'app-1';
+    const currentProject = process.env.PROJECT_NAME;
+    const lastDeployedCommit = process.env.CACHED_COMMIT_REF;
+    const latestCommit = 'HEAD';
+    const projectHasChanged = projectChanged(
+      currentProject,
+      lastDeployedCommit,
+      latestCommit
+    );
     const projectHasChanged = false;
     if (!projectHasChanged) {
       utils.build.cancelBuild(
@@ -9,3 +16,19 @@ module.exports = {
     }
   },
 };
+
+function projectChanged(currentProject, fromHash, toHash) {
+  const execSync = require('child_process').execSync;
+  const getAffected = `yarn --silent nx print-affected --base=${fromHash} --head=${toHash}`;
+  console.log(fromHash, toHash);
+  const output = execSync(getAffected).toString();
+  console.log(output);
+  //get the list of changed projects from the output
+  const changedProjects = JSON.parse(output).projects;
+  console.log(changedProjects);
+  if (changedProjects.find((project) => project === currentProject)) {
+    return true;
+  } else {
+    return false;
+  }
+}
